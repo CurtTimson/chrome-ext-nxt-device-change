195321df504a2df343f946e4f096cad4212fd20c
CurtTimson <curt@live.co.uk>
2017-03-16 09:54:28 +0000

    Add gulp task to copy dependant npm files

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,10 @@
 var gulp = require("gulp");
 
+gulp.task("copy-npm-files", function () {
+    return gulp.src(['./node_modules/designmodo-flat-ui/dist/**/**'])
+        .pipe(gulp.dest('./app/npm/designmodo-flat-ui/dist'))
+});
+
 gulp.task('publish', function(){
   return gulp.src('./app/**/**')
     .pipe(gulp.dest('dist'));
