f043e82c1cca2bc9cac11aea3b555c3cf88ede84
CurtTimson <curt@live.co.uk>
2017-03-16 08:52:06 +0000

    Update callback to reload tab

--- app/app.js
+++ app/app.js
@@ -1,64 +1,69 @@
-function getCurrentTabUrl(callback) {
-  var queryInfo = {
-    active: true,
-    currentWindow: true
-  };
-  chrome.tabs.query(queryInfo, function(tabs) {
-    var tab = tabs[0];
-    var url = tab.url;
-    console.assert(typeof url == 'string', 'tab.url should be a string');
-    callback(url);
-  });
-}
+(function(){
 
-function renderStatus(statusText) {
-  document.getElementById('status').textContent = statusText;
-}
-
-document.addEventListener('DOMContentLoaded', function() {
-
-  document.getElementById("d").addEventListener("click", function(){
-    getCurrentTabUrl(function(url) {
-      switchDevice("desktop", url);
-      return;
+  function getCurrentTabUrl(callback) {
+    var queryInfo = {
+      active: true,
+      currentWindow: true
+    };
+    chrome.tabs.query(queryInfo, function(tabs) {
+      var tab = tabs[0];
+      var url = tab.url;
+      console.assert(typeof url == 'string', 'tab.url should be a string');
+      callback(url);
     });
-  });
+  }
 
-  document.getElementById("m").addEventListener("click", function(){
-    getCurrentTabUrl(function(url) {
-      switchDevice("mobile", url);
-      return;
-    });
-  });
+  function renderStatus(statusText) {
+    document.getElementById('status').textContent = statusText;
+  }
 
-});
+  document.addEventListener('DOMContentLoaded', function() {
 
-var switchDevice = function(deviceToSwitchTo, returnUrl){
-  invokeDeviceSwitch(deviceToSwitchTo, function(){
-
-    chrome.runtime.sendMessage({redirect: returnUrl});
-  });
-}
-
-var invokeDeviceSwitch = function(deviceToSwitchTo, callback){
-
-    var url = deviceToSwitchTo;
-
-    var x = new XMLHttpRequest();
-    x.open('GET', url);
-    x.responseType = 'json';
-    x.onload = function() {
-      var response = x.response;
-      if (!response || !response.responseData || !response.responseData.results ||
-          response.responseData.results.length === 0) {
-        errorCallback('No response from server for switch!');
+    document.getElementById("d").addEventListener("click", function(){
+      getCurrentTabUrl(function(url) {
+        switchDevice("desktop", url);
         return;
+      });
+    });
+
+    document.getElementById("m").addEventListener("click", function(){
+      getCurrentTabUrl(function(url) {
+        switchDevice("mobile", url);
+        return;
+      });
+    });
+
+  });
+
+  var switchDevice = function(deviceToSwitchTo, returnUrl){
+    invokeDeviceSwitch(deviceToSwitchTo, function(){
+        renderStatus(returnUrl);
+        chrome.tabs.getSelected(null, function(tab) {
+          var code = 'window.location.reload();';
+          chrome.tabs.executeScript(tab.id, {code: code});
+          window.close();
+        });
+    });
+  }
+
+  var invokeDeviceSwitch = function(deviceToSwitchTo, callback){
+
+      var url = deviceToSwitchTo;
+
+      var x = new XMLHttpRequest();
+      x.open('GET', url);
+      x.responseType = 'json';
+      x.onreadystatechange = function()
+      {
+          if (x.readyState == 4 && x.status == 200)
+          {
+            callback();
+          }
       }
-      renderStatus("testing");
-      callback();
-    };
-    x.onerror = function() {
-      errorCallback('Network error.');
-    };
-    x.send();
-}
+      x.onerror = function() {
+        renderStatus('Network error.');
+      };
+      x.send();
+  }
+
+})();
